1089bf7718d226aa947f11fb4b70e371cf3a33ca
WijdaneCHAR <87330491+WijdaneCHAR@users.noreply.github.com>
2022-02-06 15:24:43 +0100

    code

---
+++ js/Js.js
@@ -0,0 +1,41 @@
+     //CHANGING THE COLOR OF SHOES:
+
+    let myImg0 = document.querySelector(".imgshoes");
+    let spn0 = document.querySelector(".shoes .couleurs .span1");
+    spn0.addEventListener('click', () => {
+        myImg0.src = "ressources/shoes1.png";
+    });
+
+    let myImg1 = document.querySelector(".imgshoes");
+    let spn2 = document.querySelector(".shoes .couleurs .span3");
+    spn2.addEventListener('click', () => {
+        myImg1.src = "ressources/shoes3.png";
+    });
+
+    //CHANGING THE COLOR OF THE T-SHIRT:
+
+    let myImg10 = document.querySelector(".imgtshirt");
+    let spn10 = document.querySelector(".t-shirt .couleurs .span1");
+    spn10.addEventListener('click', () => {
+        myImg10.src = "ressources/t-shirt.png";
+    });
+
+    let myImg20 = document.querySelector(".imgtshirt");
+    let spn20 = document.querySelector(".t-shirt .couleurs .span2");
+    spn20.addEventListener('click', () => {
+        myImg20.src = "ressources/t-shirt2.png";
+    });
+
+    let myImg30 = document.querySelector(".imgtshirt");
+    let spn30 = document.querySelector(".t-shirt .couleurs .span3");
+    spn30.addEventListener('click', () => {
+        myImg30.src = "ressources/t-shirt3.png";
+    });
+
+
+    let myImg40 = document.querySelector(".imgtshirt");
+    let spn40 = document.querySelector(".t-shirt .couleurs .span4");
+    spn40.addEventListener('click', () => {
+        myImg40.src = "ressources/t-shirt4.png";
+    });
+
